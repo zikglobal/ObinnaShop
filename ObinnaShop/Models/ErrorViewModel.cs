46b924ca1b8d8dca6dd70d36069f2ee810426e84
zikglobal <125207118+zikglobal@users.noreply.github.com>
2023-07-16 21:52:48 +0100

    Add project files.

---
+++ ObinnaShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ObinnaShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
